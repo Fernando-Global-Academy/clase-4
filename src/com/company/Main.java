a046e0f841090103f88a0cbed79336423e6ba847
fernandodelima <fernando.lima@mercadolibre.com>
2022-06-29 23:05:39 -0300

    Added menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        ex1();
-        ex2();
-        ex3();
+        System.out.println("Seleccione una opcion:");
+        System.out.println("1) ex1");
+        System.out.println("2) ex2");
+        System.out.println("3) ex3");
+        Scanner s = new Scanner(System.in);
+        int option = s.nextInt();
+        switch (option) {
+            case 1:
+                ex1();
+                break;
+            case 2:
+                ex2();
+                break;
+            case 3:
+                ex3();
+                break;
+        }
     }
 
     private static void ex3() {
