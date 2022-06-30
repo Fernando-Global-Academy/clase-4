3517a53fdfd4f3a0c4b6bbae3e63ce6bb07d1a4e
fernandodelima <fernando.lima@mercadolibre.com>
2022-06-29 22:47:06 -0300

    Changed ex2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        ex2();
+    }
+
+    private static void ex2() {
         System.out.println("ejercicio 2");
     }
 }
